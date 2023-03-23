dd625e2841b7531a471ca090c9c19b52e48f4ea4
dgchoi3904 <dgchoi3904@gmail.com>
2023-03-23 10:42:02 +0900

    추가 설명 기입

--- node-server-prac.js
+++ node-server-prac.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const fs = require("fs");
-
+//이것은 파일을 동적으로 작성하는 것이 아닌, 정적으로 작성된 파일들을 불러오는 클래식 서버 구동 실행문이다. 맨 마지막 listen이 실행될 경우 우리느 서버에 접속할 수 있게 된다.
 function serveStaticFile(response, path, contentType, responseCode) {
   //파일 읽기, 비동기적 방식 사용. err가 있을경우 응답 처리, 그외엔 파일 데이터를 매개변수 data에 받는다.
   fs.readFile(path, function(err, data) {
@@ -38,7 +38,9 @@ const server = http.createServer((request, response) => {
     default :
       serveStaticFile(response, './404.html', 'text/html', 200);
       break;
-
-
   }
+});
+//서버를 실행하는 구문, localhost 2080포트로 접속 할 수 있게 해준다. 콜백함수에서는 에러를 매개변수로 받을 수 있어 만일 확인하고 싶을 경우 그에 맞게 작성하면 된다.
+server.listen(2080, () => {
+  console.log('2080번 포트')
 })
