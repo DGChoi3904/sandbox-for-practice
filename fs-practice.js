be320dd91bdb162b6fb51fccf4eb37f4064d9a2d
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-12 17:40:20 +0900

    파일 시스템 사용 연습 - 잊고 있던 것.

---
+++ fs-practice.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+fs.writeFile("./hello.txt", function(err, data){
+  if(err){
+    console.error(`you got ${err} problem`);
+  }else{
+
+  }
+})
+fs.writeFile()
+console.log(fs);
